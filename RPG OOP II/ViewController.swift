43432dae8cb06c3f4270c8b850dc419c8f26c87a
David Sibrian <davidsibrian08@gmail.com>
2016-07-13 19:09:42 -0400

    Implemented character classes, buttons and labels
    Restart button not yet implemented

--- RPG OOP II/ViewController.swift
+++ RPG OOP II/ViewController.swift
@@ -7,21 +7,180 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var attackUpdateLabel: UILabel!
     
+    var attackSound: AVAudioPlayer!
+    var punchSound: AVAudioPlayer!
+    var deathSound: AVAudioPlayer!
+    var gameMusic: AVAudioPlayer!
+    
+    @IBOutlet weak var trollHPLabel: UILabel!
+    @IBOutlet weak var heroHPLabel: UILabel!
 
+    @IBOutlet weak var muteButton: UIButton!
+    @IBOutlet weak var unmuteButton: UIButton!
+    
+    @IBOutlet weak var restartButton: UIButton!
+    
+    @IBOutlet weak var heroAttackButton: UIButton!
+    @IBOutlet weak var trollAttackButton: UIButton!
+    
+    var hero: Hero!
+    var troll: Enemy!
+    
+    @IBOutlet weak var trollImageView: UIImageView!
+    @IBOutlet weak var heroImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        initializeSounds()
+        
+        troll = Enemy(startingHP: 110, initialAttackPower: 16)
+        hero = Hero(heroName: "Sir Nauticus", HP: 170, attackPower: 22)
+        
+        initializeHPLabel(troll, hero: hero)
+        
+        gameMusic.play()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    @IBAction func trollAttackButtonPressed(sender: UIButton) {
+        punchSound.play()
+        
+        if hero.attemptAttack(troll.attackPower) {
+            attackUpdateLabel.text = "\(troll.type) attacked the hero for \(troll.attackPower) damage!!"
+            heroHPLabel.text = "\(hero.HP) HP"
+        } else {
+            attackUpdateLabel.text = "The hero has dodged the attack!!! "
+        }
+        
+        if hero.isDead {
+            deathSound.play()
+            
+            attackUpdateLabel.text = "THE TROLL HAS VANQUISHED THE HERO!"
+            heroHPLabel.hidden = true
+            heroImageView.alpha = 0.5
+            
+            disableAndFadeButtons()
+        }
+    }
+    
+    @IBAction func heroAttackButtonPressed(sender: UIButton) {
+        attackSound.play()
+        
+        if troll.attemptAttack(hero.attackPower) {
+            attackUpdateLabel.text = "\(hero.name) attacks \(troll.type) for \(hero.attackPower) damage!"
+            trollHPLabel.text = "\(troll.HP) HP"
+        } else {
+            attackUpdateLabel.text = "Troll has dodged the attack by \(hero.name)!!!"
+        }
+        
+        if troll.isDead {
+            deathSound.play()
+            
+            attackUpdateLabel.text = "\(hero.name) HAS VANQUISHED THE TROLL!"
+            trollHPLabel.hidden = true
+            trollImageView.alpha = 0.5
+            
+            disableAndFadeButtons()
+        }
+    }
+    
+    @IBAction func muteButtonPressed(sender: UIButton) {
+        if gameMusic.playing {
+            gameMusic.stop()
+        }
+        muteButton.hidden = true
+        unmuteButton.hidden = false
+    }
+    
+    @IBAction func unmuteButtonPressed(sender: UIButton) {
+        if !gameMusic.playing {
+            gameMusic.play()
+        }
+        muteButton.hidden = false
+        unmuteButton.hidden = true
+    }
+    
+    @IBAction func restartButtonPressed(sender: UIButton) {
+        
+    }
+    
+    
+    func initializeHPLabel(troll: Enemy, hero: Hero) {
+        
+        trollHPLabel.text = "\(troll.HP) HP"
+        heroHPLabel.text = "\(hero.HP) HP"
+    }
+    
+    func disableAndFadeButtons() {
+        
+        trollAttackButton.enabled = false
+        trollAttackButton.alpha = 0.5
+        
+        heroAttackButton.enabled = false
+        heroAttackButton.alpha = 0.5
+        
+        restartButton.hidden = false
+    }
+    
+    func initializeSounds() {
+        
+        //initialize the attack sound (hero)
+        let attackSoundPath = NSBundle.mainBundle().pathForResource("attack", ofType: "wav")
+        let attackSoundURL = NSURL(fileURLWithPath: attackSoundPath!)
+        
+        do {
+            try attackSound = AVAudioPlayer(contentsOfURL: attackSoundURL)
+            attackSound.prepareToPlay()
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+        
+        //initialize the punch sound (troll)
+        let punchSoundPath = NSBundle.mainBundle().pathForResource("punch", ofType: "mp3")
+        let punchSoundURL = NSURL(fileURLWithPath: punchSoundPath!)
+        
+        do {
+            try punchSound = AVAudioPlayer(contentsOfURL: punchSoundURL)
+            punchSound.prepareToPlay()
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+        
+        //initialize the deathSound
+        let deathSoundPath = NSBundle.mainBundle().pathForResource("death", ofType: "wav")
+        let deathSoundURL = NSURL(fileURLWithPath: deathSoundPath!)
+        
+        do {
+            try deathSound = AVAudioPlayer(contentsOfURL: deathSoundURL)
+            deathSound.prepareToPlay()
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+        
+        //initialize the game music
+        let gameMusicPath = NSBundle.mainBundle().pathForResource("menuMusic", ofType: "mp3")
+        let gameMusicPathURL = NSURL(fileURLWithPath: gameMusicPath!)
+        
+        do {
+            try gameMusic = AVAudioPlayer(contentsOfURL: gameMusicPathURL)
+            gameMusic.prepareToPlay()
+        } catch let error as NSError {
+            print(error.debugDescription)
+        }
+        
+    }
 
 }
 
